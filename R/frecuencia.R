a4d7dbe177aa4092c059e7433ffb9fb6484e81e0
gorantesj <gorantesj@hotmail.com>
2020-12-15 00:34:54 -0600

    Estilizado con styler

--- R/frecuencia.R
+++ R/frecuencia.R
@@ -9,15 +9,16 @@
 #' @export
 #' @import dplyr ggplot2
 #' @examples
-calcular_frecuencia <- function(bd, variable, n_niveles=9, otro="Otro"){
+calcular_frecuencia <- function(bd, variable, n_niveles = 9, otro = "Otro") {
   res <- bd %>%
-    count({{variable}}) %>%
+    count({{ variable }}) %>%
     arrange(n) %>%
-    mutate({{variable}}:=if_else(row_number()<=n_niveles,
-                                as.character({{variable}}),
-                                otro)) %>%
-    group_by({{variable}}) %>%
-    summarise(n=sum(n))
+    mutate({{ variable }} := if_else(row_number() <= n_niveles,
+      as.character({{ variable }}),
+      otro
+    )) %>%
+    group_by({{ variable }}) %>%
+    summarise(n = sum(n))
   return(res)
 }
 
@@ -37,79 +38,92 @@ calcular_frecuencia <- function(bd, variable, n_niveles=9, otro="Otro"){
 #' @examples
 graficar_frecuencia <- function(bd,
                                 variable,
-                                frecuencia=n,
-                                n_niveles=9,
-                                otro="Otro",
-                                grafico="barras",
-                                color_base="#912F40"){
+                                frecuencia = n,
+                                n_niveles = 9,
+                                otro = "Otro",
+                                grafico = "barras",
+                                color_base = "#912F40") {
   resEst <- calcular_frecuencia(bd,
-                      variable = {{variable}},
-                      n_niveles = n_niveles,
-                      otro = otro)
-  g <- switch (grafico,
+    variable = {{ variable }},
+    n_niveles = n_niveles,
+    otro = otro
+  )
+  g <- switch(grafico,
     barras = frecuencia_barras(resEst,
-                               x={{variable}},
-                               y=n,
-                               color_base=color_base),
-    gota=frecuencia_gota(resEst,
-                         x={{variable}},
-                         y=n,
-                         color_base=color_base),
-    paleta=frecuencia_paleta(resEst,
-                         x={{variable}},
-                         y=n,
-                         color_base=color_base)
+      x = {{ variable }},
+      y = n,
+      color_base = color_base
+    ),
+    gota = frecuencia_gota(resEst,
+      x = {{ variable }},
+      y = n,
+      color_base = color_base
+    ),
+    paleta = frecuencia_paleta(resEst,
+      x = {{ variable }},
+      y = n,
+      color_base = color_base
+    )
   )
   return(g)
-
-
 }
 
-frecuencia_barras <- function(bd, x, y, color_base="#912F40"){
+frecuencia_barras <- function(bd, x, y, color_base = "#912F40") {
   g <- bd %>%
-    ggplot(aes(x={{x}}, y={{y}})) +
-    geom_bar(stat = "identity", fill=color_base) +
-    scale_y_continuous(labels = scales::comma_format(),
-                       name="Frecuencia") +
+    ggplot(aes(x = {{ x }}, y = {{ y }})) +
+    geom_bar(stat = "identity", fill = color_base) +
+    scale_y_continuous(
+      labels = scales::comma_format(),
+      name = "Frecuencia"
+    ) +
     # scale_x_discrete(name=stringr::str_to_sentence(eje_x)) +
-    geom_hline(yintercept = 0)+
+    geom_hline(yintercept = 0) +
     coord_flip() +
-    theme(panel.background = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid.major.x = element_line(color = "grey50",size = .1))
+    theme(
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid.major.x = element_line(color = "grey50", size = .1)
+    )
   return(g)
-
 }
 
-frecuencia_gota <- function(bd, x, y, color_base="#912F40"){
+frecuencia_gota <- function(bd, x, y, color_base = "#912F40") {
   g <- bd %>%
-    ggplot(aes(xend={{x}},x={{x}}, y=0,yend={{y}})) +
-    geom_segment(stat = "identity", color=color_base, size=10/nrow(bd), lineend = "round") +
-    scale_y_continuous(labels = scales::comma_format(),
-                       name="Frecuencia") +
+    ggplot(aes(xend = {{ x }}, x = {{ x }}, y = 0, yend = {{ y }})) +
+    geom_segment(stat = "identity", color = color_base, size = 10 / nrow(bd), lineend = "round") +
+    scale_y_continuous(
+      labels = scales::comma_format(),
+      name = "Frecuencia"
+    ) +
     coord_flip() +
-    theme(panel.background = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid.major.x = element_line(color = "grey50",size = .1))
+    theme(
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid.major.x = element_line(color = "grey50", size = .1)
+    )
   return(g)
-
 }
 
-frecuencia_paleta <- function(bd, x, y, color_base="#912F40"){
+frecuencia_paleta <- function(bd, x, y, color_base = "#912F40") {
   g <- bd %>%
     ggplot() +
-    geom_segment(aes(xend={{x}},x={{x}}, y=0,yend={{y}}), size=10/nrow(bd), lineend = "round") +
-    geom_point(aes(x={{x}}, y={{y}}),
-                 color=color_base, size=3*12/.pt)+
-    geom_text(aes(x={{x}}, y={{y}}, label=formatear_num({{y}})),
-               color="white", size=11/.pt)+
-    scale_size_area()+
-    scale_y_continuous(labels = scales::comma_format(),
-                       name="Frecuencia") +
+    geom_segment(aes(xend = {{ x }}, x = {{ x }}, y = 0, yend = {{ y }}), size = 10 / nrow(bd), lineend = "round") +
+    geom_point(aes(x = {{ x }}, y = {{ y }}),
+      color = color_base, size = 3 * 12 / .pt
+    ) +
+    geom_text(aes(x = {{ x }}, y = {{ y }}, label = formatear_num({{ y }})),
+      color = "white", size = 11 / .pt
+    ) +
+    scale_size_area() +
+    scale_y_continuous(
+      labels = scales::comma_format(),
+      name = "Frecuencia"
+    ) +
     coord_flip() +
-    theme(panel.background = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid.major.x = element_line(color = "grey50",size = .1))
+    theme(
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid.major.x = element_line(color = "grey50", size = .1)
+    )
   return(g)
-
 }
