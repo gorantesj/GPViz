49335384c25c190eb4fa512bad074574d49ac746
gorantesj <gorantesj@hotmail.com>
2020-12-21 12:47:57 -0600

    Se construyó función para distribución

--- R/distribucion.R
+++ R/distribucion.R
@@ -11,7 +11,7 @@ graficar_distribucion <- function(bd,
   return(g)
 }
 
-distribucion_histograma <- function(bd, variable, color_base, grupo){
+distribucion_histograma <- function(bd, variable, grupo, color_base){
   resumen <- bd %>% summarise(mediana=median({{variable}}),
                               maximo=median({{variable}}),
                               minimo=median({{variable}}))
@@ -25,4 +25,38 @@ distribucion_histograma <- function(bd, variable, color_base, grupo){
                color = colortools::complementary(color = color_base, plot = F)[[2]])+
     scale_x_continuous(labels = scales::comma_format())
   if(!missing(grupo)) g <- g+facet_wrap(~{{grupo}})
+}
+
+#' Title
+#'
+#' @param bd
+#' @param variable
+#' @param grupo
+#' @param color_base
+#'
+#' @return
+#' @export
+#'
+#' @examples
+distribucion_caja_violin <-function(bd, variable, grupo, color_base){
+  color_texto <- if_else(plotwidgets::col2hsl(color_base)["L",1]>.7,
+                                      "black",
+                                      "white")
+  g <- bd %>%
+    group_by({{grupo}}) %>%
+    mutate({{grupo}}:=paste({{grupo}},
+                            scales::comma(sum({{variable}})),
+                            sep = "\n")) %>%
+    ggplot(aes(x={{grupo}},
+                y={{variable}})) +
+    geom_violin(width=1, alpha = .3, fill=color_base,
+                color = colortools::complementary(color = color_base, plot = F)[[2]]) +
+    geom_boxplot(width=0.1, fill=color_base,
+                 color=colortools::complementary(color = color_base, plot = F)[[2]],
+                 alpha = .8) +
+    geom_point(
+               color = color_texto,
+               shape = 18,
+               stat="summary",fun=median)
+  return(g)
 }
--- R/temas.R
+++ R/temas.R
@@ -77,30 +77,7 @@ paleta_GP <- function(g){
     )
 }
 
-# Frecuencias
-#' Title
-#'
-#' @param g
-#'
-#' @return
-#' @export
-#'
-#' @examples
-barras_prueba <- function(g){
-  g +
-    theme(
-      # Textos
-      text = element_text(family = "Poppins",colour = "#333333"),
-      # Titulos
-      plot.title = element_text(size = 16*.75, color = "green"),
-      plot.subtitle = element_text(colour="#4D4D4D",size = 13*.75),
-      axis.title = element_text(size = 12*.75),
-      # Panel
-      panel.background = element_blank(),
-      panel.grid.major.x = element_line(color = "grey50", size = .1),
-      # Ticks
-      axis.ticks = element_blank(),
-    )
+caja_violin_GP <- function(g){
 
 }
 
