8d9f309b930679f5d8400c44d6e5da9db16d6616
Déborah <deborahandrea.m@hotmail.com>
2020-12-21 13:11:06 -0600

    merge

--- R/DM.R
+++ R/DM.R
@@ -17,8 +17,8 @@ diamonds %>%
   left_join(nn) %>% group_by(cut) %>%  mutate(m = median(carat)) %>%
   mutate(tamano = paste0(cut, "\n", n %>%  comma)) %>%
   ggplot( aes(x=fct_reorder(tamano, -m), y=carat, fill=cut)) +
-  geom_violin(width=1, alpha = .3, color = "#FFFFFF") +
-  geom_boxplot(width=0.1, color="#92ADB0", alpha = .8) +
+  geom_violin(width=1, alpha = .25, color = "#FFFFFF") +
+  geom_boxplot(width=0.1, color="#92ADB0", outlier.alpha =.3) +
   geom_point(aes(y = m), color = "#FFFFFF", shape = 18)+
   scale_fill_manual(values = c("#0D5359", "#720026", "#CE4257", "#FF7F51", "#BFBD28"))+
   # scale_color_manual(values = c("#0D5359", "#720026", "#CE4257", "#FF7F51", "#BFBD28"))+
@@ -28,4 +28,30 @@ diamonds %>%
     plot.title = element_text(size=11),
     panel.grid.minor = element_blank()
   )+
-  labs(x = "", y = "Quilates", title = "Boxplot <3 violin" )
+  labs(x = "", y = "Quilates", title = "Boxplot <3 violin" )+
+  coord_flip()
+
+graficar_distribucion <- function(bd, variable, violin = F, flip = T){
+  nn <-  bd %>%  count({{variable}})
+
+  bd %>%
+    left_join(nn) %>% group_by(cut) %>%  mutate(m = median({{variable}})) %>%
+    mutate(tamano = paste0(cut, "\n", n %>%  comma)) %>%
+    ggplot( aes(x=fct_reorder(tamano, -m), y={{variable}}, fill=cut)) +
+    geom_violin(width=1, alpha = .25, color = "#FFFFFF") +
+    geom_boxplot(width=0.1, color="#92ADB0", outlier.alpha =.3) +
+    geom_point(aes(y = m), color = "#FFFFFF", shape = 18)+
+    scale_fill_manual(values = c("#0D5359", "#720026", "#CE4257", "#FF7F51", "#BFBD28"))+
+    # scale_color_manual(values = c("#0D5359", "#720026", "#CE4257", "#FF7F51", "#BFBD28"))+
+    theme_minimal () +
+    theme(
+      legend.position="none",
+      plot.title = element_text(size=11),
+      panel.grid.minor = element_blank()
+    )+
+    labs(x = "", y = "Quilates", title = "Boxplot <3 violin" )+
+    coord_flip()
+
+}
+
+
