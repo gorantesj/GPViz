caf2210233b596655eaf8e4c6a7a49dad5f41bc7
gorantesj <gorantesj@hotmail.com>
2020-12-21 13:24:07 -0600

    tema caja_violin

--- R/temas.R
+++ R/temas.R
@@ -78,7 +78,20 @@ paleta_GP <- function(g){
 }
 
 caja_violin_GP <- function(g){
-
+  g +
+    theme(
+      # Textos
+      text = element_text(family = "Poppins",colour = "#333333"),
+      # Titulos
+      plot.title = element_text(size = 16*.75),
+      plot.subtitle = element_text(colour="#4D4D4D",size = 13*.75),
+      axis.title = element_text(size = 12*.75),
+      # Panel
+      panel.background = element_blank(),
+      panel.grid.major.x = element_line(color = "grey50", size = .1),
+      # Ticks
+      axis.ticks = element_blank(),
+    )
 }
 
 aplicar_tema <- function(g, grafico, tema){
